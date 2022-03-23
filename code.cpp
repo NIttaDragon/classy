6d171dc7f15567a15dfa94dbfbe379faf5517dd2
NittaDragon <uspemskaya@gmail.com>
2022-03-23 13:01:09 +0300

    start

--- code.cpp
+++ code.cpp
@@ -1,1 +1,49 @@
+#include <iostream>
+#include "code.h"
 
+int main()
+{
+    // Создание объекта класса Student
+    Students student;
+
+    std::string name;
+    std::string last_name;
+
+    // Ввод имени с клавиатуры
+    std::cout << "Имя: ";
+    getline(std::cin, name);
+
+    // Ввод фамилии
+    std::cout << "Фамилия: ";
+    getline(std::cin, last_name);
+
+    // Сохранение имени и фамилии в объект класса Students
+    student.set_name(name);
+    student.set_last_name(last_name);
+
+    // Оценки
+    int scores[5];
+    // Сумма всех оценок
+    int sum = 0;
+
+    // Ввод промежуточных оценок
+    for (int i = 0; i < 5; ++i) {
+        std::cout << "Оценка " << i+1 << ": ";
+        std::cin >> scores[i];
+        // суммирование
+        sum += scores[i];
+    }
+
+    // Сохраняем промежуточные оценки в объект класса Student
+    student.set_scores(scores);
+    // Считаем средний балл
+    float average_ball = sum / 5.0;
+    // Сохраняем средний балл в объект класса Students
+    student.set_average_ball(average_ball);
+    // Выводим данные по студенту
+    std::cout << "Средний балл по студенту " << student.get_name() << " "
+         << student.get_last_name() << " : "
+         << student.get_average_ball() << std::endl;
+
+    return 0;
+}
